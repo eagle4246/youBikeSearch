2011a9eb75daf98c1ecc33ea15a424e655b46076
eagle4246 <eagle4246@hotmail.com>
2025-07-11 10:28:59 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import youBikeDb from './assets/youbike-dataset';
+</script>
+
+<template>
+  <div>{{ youBikeDb }}</div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
